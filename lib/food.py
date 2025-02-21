0ff5d24ea7f8530863a837a2e9433a81028691e6
Ricardo Guerra <RikkuG491@gmail.com>
2025-02-21 14:57:57 -0500

    Added solution code

--- lib/food.py
+++ lib/food.py
@@ -1,2 +1,21 @@
 class Food:
-    pass
+    
+    all = []
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        Food.all.append(self)
+
+    @classmethod
+    def food_names(cls):
+        return [food.name for food in cls.all]
+    
+    @classmethod
+    def average_price(cls):
+        price_list = [food.price for food in cls.all]
+        return sum(price_list) / len(price_list)
+    
+    @classmethod
+    def most_expensive(cls):
+        return max(cls.all, key=lambda f:f.price)
